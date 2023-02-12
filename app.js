d8e7adc2278375f60a89473833f2555e57214908
Bikram shit <101961268+Bikramshit@users.noreply.github.com>
2023-02-12 18:06:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 
 const cors_options ={
-    origin:process.env.FRONTEND_URL,
+    origin:"http://127.0.0.1:3000",
     credentials:true,
     methods:["GET", "POST", "DELETE", "PUT"],
 }
