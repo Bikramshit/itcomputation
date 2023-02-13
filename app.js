20c026e84bfb2037369637834ea88a6414740d44
Bikram shit <101961268+Bikramshit@users.noreply.github.com>
2023-02-13 08:12:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.use(
 
 app.use(cookieParser());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 app.use(cors({
     origin:process.env.FRONTEND_URL,
